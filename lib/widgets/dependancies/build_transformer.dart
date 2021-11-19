7a609302b9c3b41c4d488cadc526d04fdb7f1a4c
TValet <t.valet@whitedev.fr>
2021-11-19 15:15:48 +0100

    Forgiving file for slider

---
+++ lib/widgets/dependancies/build_transformer.dart
@@ -0,0 +1,39 @@
+import 'package:another_transformer_page_view/another_transformer_page_view.dart';
+import 'package:flutter/material.dart';
+
+class DeepthPageTransformer extends PageTransformer {
+  DeepthPageTransformer() : super(reverse: true);
+
+  @override
+  Widget transform(Widget child, TransformInfo info) {
+    double? position = info.position;
+    if (position! <= 0) {
+      return Opacity(
+        opacity: 1.0,
+        child: Transform.translate(
+          offset: const Offset(0.0, 0.0),
+          child: Transform.scale(
+            scale: 1.0,
+            child: child,
+          ),
+        ),
+      );
+    } else if (position <= 1) {
+      const double MIN_SCALE = 0.75;
+      double scaleFactor = MIN_SCALE + (1 - MIN_SCALE) * (1 - position);
+
+      return Opacity(
+        opacity: 1.0 - position,
+        child: Transform.translate(
+          offset: Offset(0.0, info.height! * -position),
+          child: Transform.scale(
+            scale: scaleFactor,
+            child: child,
+          ),
+        ),
+      );
+    }
+
+    return child;
+  }
+}
